233ed86f906a1978fbd8743988a8dfa27dd9da42
liliyasergeeva <l.sergeeva2015@yandex.ru>
2021-05-20 02:44:15 +0700

    1Lab

---
+++ Lab/Lab/Lab.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "Container.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3) {
+        cout << "incorrect command line! "
+            "Waited: command in_file out_file"
+            << endl;
+        exit(1);
+    }
+
+    ifstream ifst(argv[1]);
+    ofstream ofst(argv[2]);
+
+    cout << "Start" << endl;
+
+    Container C; //Объявляем новый контейнер
+
+    C.In_Container(ifst); //Записываем элементы в контейнер
+
+    ofst << "Filled container. " << endl;
+
+    C.Out_Container(ofst); //Вывод контейнера
+
+    C.Clear_Container();//Очистка контейнера
+
+    ofst << "Empty container. " << endl;
+
+    C.Out_Container(ofst); //Вывод контейнера
+
+    cout << "Stop" << endl;
+    system("pause");
+    return 0;
+}
